70c400be13d36292abc24b5b22fc5626168d95e5
sh.im993366 <sh.im993366@gmail.com>
2021-08-08 20:28:16 +0900

    1520 내리막길 DFS DP

--- Backjoon/그래프/1520 내리막길.py
+++ Backjoon/그래프/1520 내리막길.py
@@ -1,4 +1,5 @@
 # 항상 내리막길로 가야하고, 최단 경로가 아니라, 경로의 개수이므로 DFS로 풀면된다.
+# 그냥 풀면 시간초과가 뜬다. 따라서 DP를 사용해야한다.
 
 import sys
 sys.setrecursionlimit(10**9)
@@ -9,23 +10,24 @@ down_road = [ list(map(int, input().split())) for _ in range(n)]
 
 direc = [(0, 1), (1, 0), (-1, 0), (0, -1)]
 
-check = [ [0]*m for _ in range(n) ]
+# DP 사용
+check = [ [-1]*m for _ in range(n) ]
 
-result = 0
+
+# check 배열에서 0이면 목적지까지 갈 수 없음. -1이면 한 번도 가본 적 없음. 1 이상의 값이면 한번 이상 가본적 있음.
 def dfs(r, c):
-    global result
 
     if (r, c) == (n-1, m-1):
-        result += 1
-    else:
-        for i in range(4):
-            nr = r + direc[i][0]
-            nc = c + direc[i][1]
-            if n > nr > -1 and m > nc > -1:
-                if down_road[r][c] > down_road[nr][nc]:
-                    check[nr][nc] = 1
-                    dfs(nr, nc)
-                    check[nr][nc] = 0
-dfs(0, 0)
-print(result)
-    
+        return 1
+    if check[r][c] != -1:
+        return check[r][c]
+    check[r][c] = 0
+    for i in range(4):
+        nr = r + direc[i][0]
+        nc = c + direc[i][1]
+        if n > nr > -1 and m > nc > -1:
+            if down_road[r][c] > down_road[nr][nc]:
+                check[r][c] += dfs(nr, nc)
+    return check[r][c]
+
+print(dfs(0, 0))
