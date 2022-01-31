8b06f00c163a667bd0bff9297ffbbde9d1da3114
Seung Hyun Im <sh.im993366@gamil.com>
2022-01-31 17:25:32 +0900

    1106 호텔, 배낭문제

--- Backjoon/KnapSack/1106 호텔.py
+++ Backjoon/KnapSack/1106 호텔.py
@@ -1,1 +1,16 @@
-# 배낭문제, 
+# 배낭문제, 홍보 비용은 정수배 만큼 늘어 날 수 있다. ( 뒤에서 줄여나가는 방식 안됨 )
+
+wanted_customer_count, n = map(int, input().split())
+
+# 홍보 비용, 고객 수
+advertisements = [ list(map(int, input().split())) for _ in range(n) ]
+
+dp_client = [float('inf')] * ( wanted_customer_count + 101 )
+dp_client[0] = 0
+
+for cost, client in advertisements:
+
+    for cli in range(client, len(dp_client)):
+        dp_client[cli] = min(dp_client[cli - client] + cost, dp_client[cli])
+
+print(min(dp_client[wanted_customer_count:]))
